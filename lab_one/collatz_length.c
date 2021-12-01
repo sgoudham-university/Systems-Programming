1bcb3fe6321e35b6ae8115fb99c55502619dbe0a
sgoudham <sgoudham@gmail.com>
2021-12-01 19:43:53 +0000

    Find Longest Collatz Sequence Given 0 < n <= 100

--- lab_one/collatz_length.c
+++ lab_one/collatz_length.c
@@ -6,6 +6,15 @@ int main() {
     printf("collatzLength(1) == %i\n", collatzLength(1));
     printf("collatzLength(16) == %i\n", collatzLength(16));
     printf("collatzLength(7) == %i\n", collatzLength(7));
+
+    int longestSequence = 0;
+    for (int i = 1; i <= 100; i++) {
+        int currentSequence = collatzLength(i);
+        if (currentSequence > longestSequence) {
+            longestSequence = i;
+        }
+    }
+    printf("\nLongest Collatz Sequence For The First 100 Numbers: %i", longestSequence);
 }
 
 int collatzLength(int n) {
